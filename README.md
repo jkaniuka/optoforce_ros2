5549c5900557e10e4a353d60fc9431b799c7e2e4
jkaniuka <kan.jan@wp.pl>
2023-05-14 16:46:22 +0200

    created Timer & added TODO in README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ROS 2 wrapper for Optoforce 3-axis force sensor (OMD-20-FE-200N)
+
+## Requirements: 
+```
+pip3 install optoforce
+```
+
+## TODO:
+
+- [ ] rqt_plot
+- [ ] add Wrench visualization in RViz
--- optoforce_wrapper/optoforce_wrapper/optoforce_node.py
+++ optoforce_wrapper/optoforce_wrapper/optoforce_node.py
@@ -3,7 +3,8 @@ from rclpy.node import Node
 from rcl_interfaces.msg import ParameterDescriptor, ParameterType
 from ast import literal_eval
 from geometry_msgs.msg import WrenchStamped
-from optoforce_driver import OptoForce16 as OptoForce
+import random
+#from optoforce import OptoForce16 as OptoForce
 
 
 class OptoforcePublisher(Node):
@@ -24,13 +25,42 @@ class OptoforcePublisher(Node):
         self.zero = self.get_parameter('zero').get_parameter_value().bool_array_value
         self.scale = literal_eval(self.get_parameter('scale').get_parameter_value().string_value)
 
-        self.sensors = [] 
+        self.sensors = ["A", "B"] 
         self.pubs = []
 
         for i in range(len(self.sensor_serial_numbers)):
             #self.sensors.append(OptoForce(port = self.port[i], speed_hz=self.speed[i], filter_hz=self.filter[i], zero=self.zero[i]))
             self.pubs.append(self.create_publisher(WrenchStamped, 'optoforce_' + self.sensor_serial_numbers[i], 10))
+
+        #timer_period = 1/max(self.speed)  # seconds
+        timer_period = 0.1
+        self.timer = self.create_timer(timer_period, self.handle_sensors)
+
         
+    
+
+    def handle_sensors(self):
+        
+        try:
+            for idx, sensor in enumerate(self.sensors):
+                msg = WrenchStamped()
+                msg.header.stamp = self.get_clock().now().to_msg()
+                msg.header.frame_id = 'force_frame'
+                #measurement = sensor.read(only_latest_data=True)
+                measurement = [random.uniform(1.0, 100.0), random.uniform(1.0, 100.0), random.uniform(1.0, 100.0)]
+                msg.wrench.force.x = measurement[0] / self.scale[idx][0]
+                msg.wrench.force.y = measurement[1] / self.scale[idx][1]
+                msg.wrench.force.z = measurement[2] / self.scale[idx][2]
+                print(msg)
+                self.pubs[idx].publish(msg)
+        except:
+            pass
+        finally:
+            # for sensor in self.sensors:
+            #     sensor.close()
+            pass
+
+
 
 
 def main(args=None):
